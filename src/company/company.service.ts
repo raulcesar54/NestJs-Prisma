986fbb98a5e69c232cc1a295e82c7a405d99bc16
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-20 05:11:18 -0300

    up

---
+++ src/company/company.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+
+@Injectable()
+export class CompanyService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createCompanyDto: CreateCompanyDto) {
+    return 'This action adds a new company';
+  }
+
+  findAll() {
+    return this.prisma.commpany.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} company`;
+  }
+
+  update(id: number, updateCompanyDto: UpdateCompanyDto) {
+    return `This action updates a #${id} company`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} company`;
+  }
+}
